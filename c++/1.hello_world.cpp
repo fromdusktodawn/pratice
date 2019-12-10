8418ca2e94f46875b558c148ad8f70fbf317e555
happyeveryday <happyeveryday@localhost.localdomain>
2019-12-11 02:20:30 +0800

    up new project

---
+++ c++/1.hello_world.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 1.hello_world.cpp
+	> Author: Happyeveryday
+	> Mail: 2563546603@qq.com
+	> Created Time: 2019年12月11日 星期三 02时18分10秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
